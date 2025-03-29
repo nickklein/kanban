d97b50128bcd1d601e695226540aca296828eefb
Nick Klein <1294247+nickklein@users.noreply.github.com>
2025-03-29 09:21:34 -0700

    style tweak

--- resources/js/Index.jsx
+++ resources/js/Index.jsx
@@ -15,14 +15,19 @@ export default function Index(props) {
             <div className={"max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"}>
                 <div className="flex bg-gray-900 min-h-screen gap-3">
                     {["To Do", "In Progress", "Done"].map((title, index) => (
-                        <div key={index} className="flex flex-col w-1/3 bg-[#161a1d] rounded-lg p-3">
-                        <h2 className="text-lg font-semibold text-gray-200 mb-4 uppercase">{title}</h2>
+                        <div key={index} className="flex flex-col w-1/3 bg-[#eceef0] rounded-lg p-3">
+                        <h2 className="text-md font-bold text-black mb-4 uppercase">{title}</h2>
                         <div className="space-y-2">
                             {[1, 2, 3].map((task) => (
-                            <div key={task} className="bg-[#22272b] hover:bg-[#637684] transition rounded-lg p-4">
-                                <p className="text-gray-300 font-medium">Task {task}</p>
+                            <div>
+                                <div key={task}
+                                    className="bg-[#fefefe] rounded-lg p-4 shadow-md transition-shadow duration-200 hover:shadow-lg hover:-translate-y-0.5"
+                                >
+                                    <p className="text-black font-medium">Task {task}</p>
+                                </div>
                             </div>
                             ))}
+                            <div className="mt-2 px-5 py-3 border-dashed text-gray-800">Add another card</div>
                         </div>
                         </div>
                     ))}
